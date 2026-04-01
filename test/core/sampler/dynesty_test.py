992b37c9103e1138d9c0c8ec51b825c8fd6d0eb3
jasper.martins <jasper.martins@aei.mpg.de>
2026-04-01 11:23:50 +0000

    implement test

--- test/core/sampler/dynesty_test.py
+++ test/core/sampler/dynesty_test.py
@@ -143,6 +143,26 @@ class TestDynesty(unittest.TestCase):
         self.assertEqual(self.dysampler.internal_sampler_next.naccept, 5)
         self.assertEqual(self.dysampler.internal_sampler_next.maxmcmc, 200)
 
+    def test_sampler_kwargs_custom(self):
+        base_sample = bilby.core.sampler.dynesty_utils.EnsembleWalkSampler
+
+        class custom_sample(base_sample):
+
+            _init_kwargs = {"custom_kwarg"}
+
+            def __init__(self, **kwargs):
+                super().__init__(**kwargs)
+                self.custom_kwarg = kwargs.get("custom_kwarg")
+
+        self.init_sampler(sample=custom_sample, naccept=5,
+                          maxmcmc=200, custom_sampler_kwargs={"custom_kwarg": 5})
+        self.assertIsInstance(
+            self.dysampler.internal_sampler_next, custom_sample
+        )
+        self.assertEqual(self.dysampler.internal_sampler_next.naccept, 5)
+        self.assertEqual(self.dysampler.internal_sampler_next.maxmcmc, 200)
+        self.assertEqual(self.dysampler.internal_sampler_next.custom_kwarg, 5)
+
     def test_run_test_runs(self):
         self.sampler._run_test()
 
